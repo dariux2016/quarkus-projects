fe1fb8f6846bc76d23599eb841dd8c3a1d9bff89
Dario De Santis <dario.desantis@eng.it>
2021-11-03 10:13:51 +0100

    getting started project

---
+++ getting-started/src/test/java/tech/donau/course/NativeGreetingResourceIT.java
@@ -0,0 +1,9 @@
+package tech.donau.course;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeGreetingResourceIT extends GreetingResourceTest {
+
+    // Execute the same tests but in native mode.
+}
